ebb34b06ef767490c058a266681c634a1d0d444b
leo Le Diouron <leo.le-diouron@epitech.eu>
2017-05-27 17:06:48 +0200

    --help

--- Server/src/main.c
+++ Server/src/main.c
@@ -5,14 +5,17 @@
 ** Login   <le-mou_t@epitech.net>
 ** 
 ** Started on  Sat May 27 16:26:33 2017 Thomas LE MOULLEC
-** Last update Sat May 27 16:26:44 2017 Thomas LE MOULLEC
+** Last update Sat May 27 17:06:20 2017 Leo Le Diouron
 */
 
 #include "server.h"
 
 int main(int ac, char **av)
 {
-  (void)ac;
-  (void)av;
+  if (ac == 2 && strcmp(av[1], "--help") == 0)
+    {
+      printf("USAGE ./server port\n");
+      return (0);
+    }
   return (0);
 }
